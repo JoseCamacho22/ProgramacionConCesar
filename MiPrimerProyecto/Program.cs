0e230337ff0254d5854dc7e3e96463d198d8dd96
Jose Camacho <jacamacho@gfi.es>
2018-03-14 14:22:50 +0100

    Trabajando con operadores de Asignacion

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -50,9 +50,21 @@ namespace MiPrimerProyecto
 
             #endregion
 
-
             Console.ReadLine();
-          
+
+            #region Operadores de Asignacion
+            Console.WriteLine("Introduce un numero entero B");
+            //como el usuario ingresa un string tengo que parsearlo.
+            int numB = Int32.Parse(Console.ReadLine());
+            suma += numB;
+            Console.WriteLine("el nuevo valor de suma es = " + suma);
+            resta -= numB;
+            Console.WriteLine("el nuevo valor de la resta  es = " + resta);
+            multi *= numB;
+            Console.WriteLine("el nuevo valor de la multiplicacion  es = " + multi);
+            #endregion
+
+
 
             #endregion
 
