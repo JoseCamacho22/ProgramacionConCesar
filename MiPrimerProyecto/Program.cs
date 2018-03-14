a63320d7fea0800032d7c0c4c0aaa8691a4f4fb5
Jose Camacho <jacamacho@gfi.es>
2018-03-14 14:13:05 +0100

    Trabajando con operadores Matematicos

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -28,8 +28,28 @@ namespace MiPrimerProyecto
             #region Imprimir
             Console.WriteLine("Probando mi primer proyecto con C#");
 
-            Console.WriteLine("Mes Actual " + Meses.Enero);
-          
+            const int miConst = 10;
+            Console.WriteLine("Introduce un numero entero");
+            //como el usuario ingresa un string tengo que parsearlo.
+            int num =Int32.Parse( Console.ReadLine());
+
+            #region Operadores Matematicos
+            int suma = miConst + num;
+            Console.WriteLine("suma = " + suma);
+            
+
+            int resta = miConst - num;
+            Console.WriteLine("Resta = " + resta);
+
+            int opResta = -resta;
+            Console.WriteLine("Opuesto resta  = " + opResta);
+
+
+            int multi = miConst * num;
+            Console.WriteLine("Multiplicacion = " + multi);
+
+            #endregion
+
 
             Console.ReadLine();
           
